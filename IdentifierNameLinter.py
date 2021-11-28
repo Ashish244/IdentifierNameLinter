cedb405cb901de90cd8e0aa5aa0f425e9930c79a
Ashish244 <69396847+Ashish244@users.noreply.github.com>
2021-11-27 18:52:07 -0600

    first

--- IdentifierNameLinter.py
+++ IdentifierNameLinter.py
@@ -18,41 +18,38 @@ for f1, f2, f3 in os.walk(r'files_from_git'):
             javascriptLinks.append(os.path.join(f1, file))
         if file.endswith('.rb'):
             rubyLinks.append(os.path.join(f1, file))
+
+        #if file.endswith('.js'):
+            #javascriptLinks.append(os.path.join(f2, file))
+        #if file.endswith('.rb'):
+            #rubyLinks.append(os.path.join(f2, file))
 i = 0
 python_text=[]
-while i<len(pythonLinks):
-    with open(pythonLinks[i]) as op:
-        try:
-            contents = op.read()
-            py_text.append(contents)
-        except:
-            pass
-    i=i+1
 Language.build_library('build/my-languages.so',
                        ['tree-sitter-go', 'tree-sitter-javascript', 'tree-sitter-python', 'tree-sitter-ruby'])
-py_parser=Parser()
-PYTHON_LANGUAGE=Language('build/my-languages.so', 'python')
-py_parser.set_language(PYTHON_LANGUAGE)
+pythonparser=Parser()
+PY_LANGUAGE=Language('build/my-languages.so','python')
+pythonparser.set_language(PY_LANGUAGE)
 def parser(source, parser1):
-    list1 = []
+    list1=[]
     def node_parser(root1):
-        if len(root1.children) != 0:
+        if len(root1.children)!=0:
             for j in root1.children:
-                if j.type == 'identifier':
+                if j.type=='identifier':
                     list1.append(j)
                 node_parser(j)
+                #list1.append(i)
         else:
             return
-    tree_name = parser1.parse(bytes(source, "utf8"))
-    root_node = tree_name.root_node
-    node_parser(root_node)
+    Treename=parser1.parse(bytes(source, "utf8"))
+    Root_node=Treename.Root_node
+    node_parser(Root_node)
     return list1
-    for i in range(len(py_text)):   //to print
-         print("output for", py_paths[i])
-        py_list = parser(py_text[i], py_parser)
-        code = py_text[i].split('\n')
-        ids = []
-        for e in py_list:
-            row = e.start_point[0]
-            col = e.start_point[1]
-             print(code[row][col:e.end_point[1]], "Row Num", row, " Column Num", col)
+    for i in range(len(python_text)):
+         print("output for", pythonpaths[i])
+        PythonList = parser(python_text[i], py_parser)
+        code = python_text[i].split('\n')
+        for e in PythonList:
+            row=e.start_point[0]
+            col=e.start_point[1]
+             print(code[row][col:e.end_point[1]],"Row Num",row,"Column Num",col)
